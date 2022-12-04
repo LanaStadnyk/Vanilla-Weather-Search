fa4eaed08ed389a75dd74578b1cfd4cda563a7b4
LanaStadnyk <svetlanastadnyk@gmail.com>
2022-12-05 00:19:32 +0100

    Added the current date and temperature

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
-function displayTemperature (response) {
-    console.log(response.data);
-    document.querySelector("#temperature").innerHTML = Math.round(response.data.temperature.current);
-    document.querySelector("#humidity").innerHTML = response.data.temperature.humidity;
-    document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
-    document.querySelector("#condition").innerHTML = response.data.condition.description;
-
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    };
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    };
+    return ` ${day} ${hours}:${minutes}`;
 }
 
-let apiKey = "124ftfab6b55c54beo58d91354585001"
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Odesa&key=${apiKey}&units=metric`;
+function displayTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#condition").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#date").innerHTML = formatDate(response.data.time * 1000);
+}
+
+let apiKey = "124ftfab6b55c54beo58d91354585001";
+let city = "Odesa";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
