4abe9186d90e74fca5a4f226f149043a379c0d87
LanaStadnyk <svetlanastadnyk@gmail.com>
2022-12-05 20:17:12 +0100

    Added the active class to the units

--- src/app.js
+++ src/app.js
@@ -1,23 +1,30 @@
 function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = days[date.getDay()];
-    let hours = date.getHours();
-    if (hours < 10) {
-        hours = `0${hours}`;
-    };
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-        minutes = `0${minutes}`
-    };
-    return ` ${day} ${hours}:${minutes}`;
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return ` ${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
   celsiusTemperature = response.data.temperature.current;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    celsiusTemperature
-  );
+  document.querySelector("#temperature").innerHTML =
+    Math.round(celsiusTemperature);
   document.querySelector("#humidity").innerHTML =
     response.data.temperature.humidity;
   document.querySelector("#wind-speed").innerHTML = Math.round(
@@ -26,15 +33,21 @@ function displayTemperature(response) {
   document.querySelector("#condition").innerHTML =
     response.data.condition.description;
   document.querySelector("#city").innerHTML = response.data.city;
-  document.querySelector("#date").innerHTML = formatDate(response.data.time * 1000);
-  document.querySelector("#weather-icon").setAttribute("src", `${response.data.condition.icon_url}`);
-  document.querySelector("#weather-icon").setAttribute("alt", `${response.data.condition.description}`);
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.time * 1000
+  );
+  document
+    .querySelector("#weather-icon")
+    .setAttribute("src", `${response.data.condition.icon_url}`);
+  document
+    .querySelector("#weather-icon")
+    .setAttribute("alt", `${response.data.condition.description}`);
 }
 
 function search(city) {
-let apiKey = "124ftfab6b55c54beo58d91354585001";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+  let apiKey = "124ftfab6b55c54beo58d91354585001";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
@@ -45,19 +58,30 @@ function handleSubmit(event) {
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
-  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature * 9 / 5 + 32);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    (celsiusTemperature * 9) / 5 + 32
+  );
+  document.querySelector("#fahrenheit-link").classList.add("active");
+  document.querySelector("#celsius-link").classList.remove("active");
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
+  document.querySelector("#temperature").innerHTML =
+    Math.round(celsiusTemperature);
+    document.querySelector("#celsius-link").classList.add("active");
+    document.querySelector("#fahrenheit-link").classList.remove("active");
 }
 
-celsiusTemperature = null; 
+celsiusTemperature = null;
 
 document.querySelector("#search-form").addEventListener("submit", handleSubmit);
 
-document.querySelector("#fahrenheit-link").addEventListener("click", displayFahrenheitTemperature);
-document.querySelector("#celsius-link").addEventListener("click", displayCelsiusTemperature);
+document
+  .querySelector("#fahrenheit-link")
+  .addEventListener("click", displayFahrenheitTemperature);
+document
+  .querySelector("#celsius-link")
+  .addEventListener("click", displayCelsiusTemperature);
 
 search("Odesa");
